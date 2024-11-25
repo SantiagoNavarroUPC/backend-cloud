cbb2f0877b39e70b7cc01017d609eea64d0e2208
SantiagoNavarroUPC <snavarroc@unicesar.edu.co>
2024-11-25 15:14:36 -0500

    commit

--- src/infrastructure/db/connection.js
+++ src/infrastructure/db/connection.js
@@ -1,11 +1,15 @@
-const db = require('./mysql'); // Asegúrate de la ruta correcta
+const mysql = require('mysql2/promise');
 
 (async () => {
-  try {
-    const connection = await db.getConnection();
-    console.log('Conexión exitosa a la base de datos MySQL');
-    connection.release(); // Liberar la conexión al pool
-  } catch (error) {
-    console.error('Error al conectar a la base de datos:', error.message);
-  }
+  const connection = await mysql.createConnection({
+    host: 'localhost', // Cambia esto si es necesario
+    user: 'root',
+    password: 'rootpassword',
+    database: 'backend_cloud',
+  });
+
+  const [rows] = await connection.query('SHOW TABLES');
+  console.log(rows);
+  connection.end();
 })();
+
