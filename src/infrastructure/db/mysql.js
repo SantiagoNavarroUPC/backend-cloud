124b6a0d26b3367de192e38708ecb01615076e6c
SantiagoNavarroUPC <snavarroc@unicesar.edu.co>
2024-11-25 13:21:20 -0500

    commit

--- src/infrastructure/db/mysql.js
+++ src/infrastructure/db/mysql.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2/promise');
 require('dotenv').config(); // Carga variables desde .env
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
+  host: process.env.DB_HOST || 'mysql',
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || 'rootpassword',
   database: process.env.DB_NAME || 'backend_cloud',
